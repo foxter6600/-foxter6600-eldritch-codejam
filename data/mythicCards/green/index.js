2b9e738b6d723b9f3026f056aab734a60d56b9ef
foxter6600 <foxter6600@gmail.com>
2022-08-28 15:59:54 +0300

    feat: add all features exept difficulties

---
+++ data/mythicCards/green/index.js
@@ -0,0 +1,114 @@
+import greenCards from '../../../assets/MythicCards/green/index.js'
+
+const greenCardsData = [
+  {
+    id: 'green1',
+    cardFace: greenCards.green1,
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green2',
+    cardFace: greenCards.green2,
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green3',
+    cardFace: greenCards.green3,
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green4',
+    cardFace: greenCards.green4,
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green5',
+    cardFace: greenCards.green5,
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green6',
+    cardFace: greenCards.green6,
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green7',
+    cardFace: greenCards.green7,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green8',
+    cardFace: greenCards.green8,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green9',
+    cardFace: greenCards.green9,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green10',
+    cardFace: greenCards.green10,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green11',
+    cardFace: greenCards.green11,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green12',
+    cardFace: greenCards.green12,
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green13',
+    cardFace: greenCards.green13,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green14',
+    cardFace: greenCards.green14,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green15',
+    cardFace: greenCards.green15,
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green16',
+    cardFace: greenCards.green16,
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green17',
+    cardFace: greenCards.green17,
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green18',
+    cardFace: greenCards.green18,
+    difficulty: 'easy',
+    color: 'green',
+  },
+]
+
+export default greenCardsData
---
+++ data/mythicCards/index.js
@@ -0,0 +1,7 @@
+import brownCardsData from '../mythicCards/brown/index.js'
+import blueCardsData from '../mythicCards/blue/index.js'
+import greenCardsData from '../mythicCards/green/index.js'
+
+const deck = { brownCardsData, blueCardsData, greenCardsData }
+
+export default deck
